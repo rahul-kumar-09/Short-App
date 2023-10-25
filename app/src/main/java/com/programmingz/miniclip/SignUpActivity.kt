8771232e34e2e145b3019086b0b2c55e24559832
Rahul kumar maury <rahu7827759@gmail.com>
2023-10-25 14:56:42 +0530

    SignUp Screen

---
+++ app/src/main/java/com/programmingz/miniclip/SignUpActivity.kt
@@ -0,0 +1,81 @@
+package com.programmingz.miniclip
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.programmingz.miniclip.databinding.ActivitySignUpBinding
+import com.programmingz.miniclip.model.UserModel
+import com.google.firebase.ktx.Firebase
+import com.programmingz.miniclip.util.UiUtil
+
+class SignUpActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.submitBtn.setOnClickListener {
+            signup()
+        }
+    }
+
+    fun setInProgress(inProgress: Boolean){
+        if (inProgress){
+            binding.progressBar.visibility = View.VISIBLE
+            binding.submitBtn.visibility = View.GONE
+        } else {
+            binding.progressBar.visibility = View.GONE
+            binding.submitBtn.visibility = View.VISIBLE
+        }
+    }
+
+    fun signup(){
+        val email = binding.emailInput.text.toString()
+        val password = binding.passwordInput.text.toString()
+        val confirmPassword = binding.confirmPasswordInput.text.toString()
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.emailInput.setError("Email not valid")
+            return
+        }
+        if (password.length<6){
+            binding.passwordInput.setError("Minimum 6 characters")
+            return
+        }
+        if (password!=confirmPassword){
+            binding.confirmPasswordInput.setError("Password not matched")
+            return
+        }
+        signupWithFirebase(email, password)
+    }
+
+    fun signupWithFirebase(email: String, password: String){
+        setInProgress(true)
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+            email, password
+        ).addOnSuccessListener {
+            it.user?.let {user ->
+                val userModel = UserModel(user.uid,email,email.substringBefore("@"))
+                Firebase.firestore.collection("users")
+                    .document(user.uid)
+                    .set(userModel).addOnSuccessListener {
+                        UiUtil.showToast(applicationContext,"Account created successfully")
+                            setInProgress(false)
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+                    }
+            }
+        }.addOnFailureListener {
+            UiUtil.showToast(applicationContext,it.localizedMessage?: "Something went wrong")
+            setInProgress(false)
+        }
+    }
+
+}
