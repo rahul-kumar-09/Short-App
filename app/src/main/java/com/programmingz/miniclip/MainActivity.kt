dca44303b7888a64a539e5275c45e041cfcf900f
Rahul kumar maury <rahu7827759@gmail.com>
2023-10-29 12:57:33 +0530

    SignUp Screen Add Login Screen Add Bottom Navigation

--- app/src/main/java/com/programmingz/miniclip/MainActivity.kt
+++ app/src/main/java/com/programmingz/miniclip/MainActivity.kt
@@ -2,10 +2,30 @@ package com.programmingz.miniclip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.programmingz.miniclip.databinding.ActivityMainBinding
+import com.programmingz.miniclip.util.UiUtil
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavBar.setOnItemReselectedListener {menuItem->
+            when(menuItem.itemId){
+                R.id.bottom_menu_home -> {
+                    UiUtil.showToast(this, "Home")
+                }
+                R.id.bottom_menu_add_video -> {
+                    UiUtil.showToast(this, "Add Video")
+                }
+                R.id.bottom_menu_profile -> {
+                    UiUtil.showToast(this, "Profile")
+                }
+            }
+            false
+        }
     }
 }
