35b06d8a809a3b8a582347117a001afb32bf615b
Rahul kumar maury <rahu7827759@gmail.com>
2023-10-31 21:38:19 +0530

    SignUp Screen Add Login Screen Add Bottom Navigation Add video upload option

--- app/src/main/java/com/programmingz/miniclip/VideoUploadActivity.kt
+++ app/src/main/java/com/programmingz/miniclip/VideoUploadActivity.kt
@@ -7,11 +7,19 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.google.firebase.Timestamp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.programmingz.miniclip.databinding.ActivityVideoUploadBinding
+import com.programmingz.miniclip.model.VideoModel
 import com.programmingz.miniclip.util.UiUtil
 
 class VideoUploadActivity : AppCompatActivity() {
@@ -27,12 +35,79 @@ class VideoUploadActivity : AppCompatActivity() {
         videoLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result->
             if (result.resultCode == RESULT_OK){
                 selectedVideoUrl = result.data?.data
-                UiUtil.showToast(this,"Got Video" + selectedVideoUrl.toString())
+                showPostView()
             }
         }
 
         binding.uploadView.setOnClickListener {
             checkPermissionAndOpenVideoPicker()
+        }
+        binding.submitPostBtn.setOnClickListener {
+            postVideo()
+        }
+        binding.cancelPostBtn.setOnClickListener {
+                finish()
+        }
+    }
+
+    private fun postVideo() {
+        if (binding.postCaptionInput.text.toString().isEmpty()){
+            binding.postCaptionInput.setError("Write something")
+            return
+        }
+        setInProgress(true)
+        selectedVideoUrl?.apply {
+            //store in firebase cloud storage
+            val videoRef = FirebaseStorage.getInstance()
+                .reference
+                .child("videos/"+ this.lastPathSegment)
+
+            videoRef.putFile(this)
+                .addOnSuccessListener {
+                    videoRef.downloadUrl.addOnSuccessListener {downloadUrl->
+                        postToFirestore(downloadUrl.toString())
+                    }
+                }
+            //video model store in firebase firestore
+        }
+    }
+
+    private fun postToFirestore(url: String){
+        val videoModel = VideoModel(
+        FirebaseAuth.getInstance().currentUser?.uid!! + "_" + Timestamp.now().toString(),
+        binding.postCaptionInput.text.toString(),
+        url,
+        FirebaseAuth.getInstance().currentUser?.uid!!,
+        Timestamp.now(),
+        )
+        Firebase.firestore.collection("videos")
+            .document(videoModel.videoId)
+            .set(videoModel)
+            .addOnSuccessListener {
+                setInProgress(true)
+                UiUtil.showToast(applicationContext,"Video uploaded")
+                finish()
+            }.addOnFailureListener {
+                setInProgress(true)
+                UiUtil.showToast(applicationContext, "Video failed to upload")
+            }
+    }
+
+    private fun setInProgress(inProgress: Boolean){
+        if (inProgress){
+            binding.progressBar.visibility = View.VISIBLE
+            binding.submitPostBtn.visibility = View.GONE
+        } else {
+            binding.progressBar.visibility = View.GONE
+            binding.submitPostBtn.visibility = View.VISIBLE
+        }
+    }
+
+    private fun showPostView(){
+        selectedVideoUrl?.let {
+            binding.postView.visibility = View.VISIBLE
+            binding.uploadView.visibility = View.GONE
+            Glide.with(binding.postThumbnailView).load(it).into(binding.postThumbnailView)
         }
     }
 
---
+++ app/src/main/java/com/programmingz/miniclip/model/VideoModel.kt
@@ -0,0 +1,11 @@
+package com.programmingz.miniclip.model
+
+import com.google.firebase.Timestamp
+
+data class VideoModel(
+    var videoId: String ="",
+    var title: String ="",
+    var url: String ="",
+    var uploaderId: String ="",
+    var createdTime: Timestamp = Timestamp.now()
+)
