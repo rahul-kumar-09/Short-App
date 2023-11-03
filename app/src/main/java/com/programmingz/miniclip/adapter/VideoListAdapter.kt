3b1fd0b9ce1fc4eeec9bfaf83c9b2c766166b3df
Rahul kumar maury <rahu7827759@gmail.com>
2023-11-03 21:38:09 +0530

    SignUp Screen Add Login Screen Add Bottom Navigation Add video upload option Complete MainActivity (connected with firebase firestore) add profile icon on video, progress bar and caption

--- app/src/main/java/com/programmingz/miniclip/adapter/VideoListAdapter.kt
+++ app/src/main/java/com/programmingz/miniclip/adapter/VideoListAdapter.kt
@@ -1,12 +1,19 @@
 package com.programmingz.miniclip.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.programmingz.miniclip.R
 import com.programmingz.miniclip.databinding.VideoItemRowBinding
+import com.programmingz.miniclip.model.UserModel
 import com.programmingz.miniclip.model.VideoModel
 
 class VideoListAdapter(
@@ -16,10 +23,30 @@ class VideoListAdapter(
     inner class VideoViewHolder(private val binding: VideoItemRowBinding) : RecyclerView.ViewHolder(binding.root){
 
         fun bindVideo(videoModel: VideoModel){
+            //bind user data
+            Firebase.firestore.collection("users")
+                .document(videoModel.uploaderId)
+                .get().addOnSuccessListener {
+                    val userModel = it?.toObject(UserModel::class.java)
+                    userModel?.apply {
+                        binding.usernameView.text = username
+                        //binding profile picture
+                        Glide.with(binding.profileIcon).load(profilePic)
+                            .circleCrop()
+                            .apply(
+                                RequestOptions().placeholder(R.drawable.icon_person)
+                                )
+                            .into(binding.profileIcon)
+                    }
+                }
+            binding.captionView.text = videoModel.title
+            binding.progressBar.visibility = View.VISIBLE
+
             //bindVideo
             binding.videoView.apply {
                 setVideoPath(videoModel.url)
                 setOnPreparedListener {
+                    binding.progressBar.visibility = View.GONE
                     it.start()
                     it.isLooping = true
                 }
